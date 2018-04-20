c0ebdc97bc1dccd8ee1d2cd4c883eab2f9999a01
caohang <caohanghust@gmail.com>
2018-04-20 11:10:35 +0800

    纹理

--- pages/demo4-texture/main.js
+++ pages/demo4-texture/main.js
@@ -1,8 +1,9 @@
 /**
- * @file   绘制网格
+ * @file   魔方完整版
  * @author caohang (caohanghust@gmail.com)
  * @date   2018/3/29
  */
+
 
 let demo = {
     init() {
@@ -12,65 +13,111 @@ let demo = {
         this.initLight();
         this.initObject();
         this.initTween();
+        // this.initRaycaster();
         this.animation();
     },
     initThree() {
-        width = document.getElementById('canvas-frame').clientWidth;
-        height = document.getElementById('canvas-frame').clientHeight;
+        this.width = document.getElementById('canvas-frame').clientWidth;
+        this.height = document.getElementById('canvas-frame').clientHeight;
         this.renderer = new THREE.WebGLRenderer({
             antialias : true
         });
-        this.renderer.setSize(width, height);
+        this.renderer.setSize(this.width, this.height);
         document.getElementById('canvas-frame').appendChild(this.renderer.domElement);
-        this.renderer.setClearColor(0xFFFFFF, 1.0);
+        this.renderer.setClearColor(0x000000, 1.0);
+        this.renderer.shadowMapEnabled = true;
+        this.clock = new THREE.Clock();
     },
     initCamera() {
-        this.camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-        this.camera.position.x = 0;
-        this.camera.position.y = 0;
-        this.camera.position.z = 600;
-        this.camera.up.x = 0;
-        this.camera.up.y = 1;
-        this.camera.up.z = 0;
+        this.camera = new THREE.PerspectiveCamera(60, this.width / this.height, .1, 1000);
+        this.camera.position.x = -300;
+        this.camera.position.y = 400;
+        this.camera.position.z = 300;
         this.camera.lookAt(this.scene.position);
+
+        this.trackballControls = new THREE.TrackballControls(this.camera);
+        this.trackballControls.rotateSpeed = 3;
+        this.trackballControls.zoomSpeed = 1;
+        this.trackballControls.panSpeed = 2;
     },
     initScene() {
         this.scene = new THREE.Scene();
+        // this.scene.fog = new THREE.Fog(0xffffff, .015, 100);
     },
     initLight() {
-        // let light = new THREE.AmbientLight(0x00FF00);
-        // light.position.set(100, 100, 200);
-        // this.scene.add(light);
-        //
-        // // 第二个参数是光源强度，你可以改变它试一下
-        // let light2 = new THREE.DirectionalLight(0xff0000,1);
-        // // 位置不同，方向光作用于物体的面也不同，看到的物体各个面的颜色也不一样
-        // light2.position.set(0,0,1);
-        // this.scene.add(light2);
-        //
-        // let light3 = new THREE.PointLight(0x0000FF);
-        // light3.position.set(0, 0, 500);
-        // this.scene.add(light3);
+        let spotLight = new THREE.SpotLight(0xffffff);
+        spotLight.position.set(-60, 60, 60);
+        spotLight.castShadow = true;
+        this.scene.add(spotLight);
     },
     initObject() {
-        var geometry = new THREE.CubeGeometry( 200, 100, 50,4,4);
-        let texture = THREE.ImageUtils.loadTexture("texture.jpg",null,function(t) {});
-        var material = new THREE.MeshBasicMaterial({map:texture});
-        this.mesh = new THREE.Mesh( geometry,material);
-        this.mesh.position = new THREE.Vector3(0,0,0);
-        this.scene.add(this.mesh);
+        let black = 0x000000;
+        let red = 0xff0000;
+        let green = 0x00ff00;
+        let blue = 0x0000ff;
+        // 坐标轴
+        let axes = new THREE.AxisHelper(2000);
+        this.scene.add(axes);
+
+        let ballGemo = new THREE.SphereGeometry(200, 50, 50);
+        let texture = THREE.ImageUtils.loadTexture('floor-wood.jpg');
+        let material = new THREE.MeshBasicMaterial({
+            // color: 0xff0000,
+            // wireframe: true,
+            // wireframeLinewidth: 1
+            map: texture
+        });
+        let ball = new THREE.Mesh(ballGemo, material);
+        this.scene.add(ball);
+
+
     },
+
+    faces(color, size) {
+        let solidWidth = size / 10;
+        let canvas = document.createElement('canvas');
+        canvas.width = size;
+        canvas.height = size;
+        let context = canvas.getContext('2d');
+        if (context) {
+            context.fillStyle = 'rgba(0, 0, 0, 1)';
+            context.fillRect(0, 0, size, size);
+            context.rect(solidWidth, solidWidth, size - solidWidth * 2, size - solidWidth * 2);
+            context.lineJoin = 'round';
+            context.lineWidth = solidWidth;
+            context.fillStyle = color;
+            context.strokeStyle = color;
+            context.stroke();
+            context.fill();
+        }
+        return canvas;
+    },
+
     initTween() {
-        new TWEEN.Tween( this.mesh.position)
-            .to( { x: -400 }, 3000 ).repeat( Infinity ).start();
+        // new TWEEN.Tween(this.selected.rotation)
+        //     .to( { x: Math.PI / 2 }, 1000 ).repeat( 0 ).start();
     },
+    initRaycaster() {
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
+        window.addEventListener('mousemove', e => {
+            this.mouse.x = (e.clientX / this.width) * 2 - 1;
+            this.mouse.y = -(e.clientY / this.height) * 2 + 1;
+        }, false);
+    },
+
     animation() {
-        //renderer.clear();
-        // this.render.clear();
-        // this.camera.position.x = this.camera.position.x +1;
+        let delta = this.clock.getDelta();
+        this.trackballControls.update(delta);
+        // this.selected.rotation.x += .01;
+        // for (let i = 0; i < 27; i += 3) {
+        //     rotateAroundWorldZ(this.rubik.children[i], .01);
+        // }
         this.renderer.render(this.scene, this.camera);
         requestAnimationFrame(this.animation.bind(this));
+
         TWEEN.update();
+
     }
 };
 demo.init();
