66d476a7588ba955df076dd7ec5da08ffc902d98
caohang002 <caohang002@ljstaff.com>
2018-09-21 18:06:08 +0800

    renderorder

---
+++ pages/demo22-rendererOrder/main.js
@@ -0,0 +1,118 @@
+/**
+ * @file   魔方完整版
+ * @author caohang (caohanghust@gmail.com)
+ * @date   2018/3/29
+ */
+
+
+let demo = {
+    init() {
+        this.initThree();
+        this.initScene();
+        this.initCamera();
+        this.initLight();
+        this.initObject();
+        this.initTween();
+        this.initRaycaster();
+        this.animation();
+    },
+    initThree() {
+        this.width = document.getElementById('canvas-frame').clientWidth;
+        this.height = document.getElementById('canvas-frame').clientHeight;
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true
+        });
+        this.renderer.setSize(this.width, this.height);
+        document.getElementById('canvas-frame').appendChild(this.renderer.domElement);
+        this.renderer.setClearColor(0x000000, .5);
+        this.renderer.shadowMapEnabled = true;
+        this.clock = new THREE.Clock();
+    },
+    initCamera() {
+        this.camera = new THREE.PerspectiveCamera(60, this.width / this.height, .1, 1000);
+        this.camera.position.x = -300;
+        this.camera.position.y = 400;
+        this.camera.position.z = 300;
+        this.camera.lookAt(this.scene.position);
+
+        this.trackballControls = new THREE.TrackballControls(this.camera);
+        this.trackballControls.rotateSpeed = 3;
+        this.trackballControls.zoomSpeed = 1;
+        this.trackballControls.panSpeed = 2;
+    },
+    initScene() {
+        this.scene = new THREE.Scene();
+        // this.scene.fog = new THREE.Fog(0xffffff, .015, 100);
+    },
+    initLight() {
+        let spotLight = new THREE.SpotLight(0xffffff);
+        spotLight.position.set(-60, 60, 60);
+        spotLight.castShadow = true;
+        this.scene.add(spotLight);
+    },
+    initObject() {
+        let black = 0x000000;
+        let red = 0xff0000;
+        let green = 0x00ff00;
+        let blue = 0x0000ff;
+        // 坐标轴
+        let axes = new THREE.AxisHelper(2000);
+        this.scene.add(axes);
+
+        const boxGeom = new THREE.BoxGeometry(100, 100, 100)
+        const boxMatrial = new THREE.MeshBasicMaterial({
+          color: 0xff0000,
+          transparent: true,
+          opacity: 1,
+          depthTest:2
+        })
+        const box = new THREE.Mesh(boxGeom, boxMatrial)
+        box.renderOrder = 1
+        this.scene.add(box)
+
+        let boxGeom1 = new THREE.BoxGeometry(50, 50, 50)
+        const boxMatrial1 = new THREE.MeshBasicMaterial({
+          color: 0x00ff00,
+          transparent: true,
+          depthTest: 2,
+          opacity: 1
+        })
+        const box1 = new THREE.Mesh(boxGeom1, boxMatrial1)
+        box1.position.set(100 , 100, 100)
+        // box1.material.color = 0x00ff00
+        box1.renderOrder = 2
+        this.scene.add(box1)
+        setTimeout(() => {
+            this.renderer.clearDepth()
+        }, 1000)
+
+
+
+        // const cameraHelper = new THREE.CameraHelper(this.camera)
+        // this.scene.add(cameraHelper)
+    },
+    initTween() {
+
+    },
+    initRaycaster() {
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
+        window.addEventListener('mousemove', e => {
+            this.mouse.x = (e.clientX / this.width) * 2 - 1;
+            this.mouse.y = -(e.clientY / this.height) * 2 + 1;
+        }, false);
+    },
+
+    animation() {
+        let delta = this.clock.getDelta();
+        this.trackballControls.update(delta);
+
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(this.animation.bind(this));
+
+        TWEEN.update();
+
+    }
+};
+demo.init();
